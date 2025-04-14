2ed2a80518513169f851008015f5907668f606bb
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2025-04-14 18:05:52 +0000

    Brute Force

---
+++ leetcode 217 Contains Duplicate /1_BruteForce.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        boolean dupF = false ;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i+1 ; j < n ; j++){
+                if(nums[i]==nums[j]){
+                    dupF = true ;
+                    break ;
+                }
+            }
+             if(dupF) break ;
+    }
+
+ return dupF;
+        }
+
+ 
+}
