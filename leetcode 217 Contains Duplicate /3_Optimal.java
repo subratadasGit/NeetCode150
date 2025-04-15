89a905cc96edb050ec81774abb8299e30675b026
Subrata Das <158165979+subratadasGit@users.noreply.github.com>
2025-04-15 08:46:49 +0000

    Optimal Approach

---
+++ leetcode 217 Contains Duplicate /3_Optimal.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seenNumbers = new HashSet<>();
+        for(int num: nums){
+            if(seenNumbers.contains(num)){
+                return true ;
+            }
+            seenNumbers.add(num);
+        }
+        return false ;
+
+    }
+}
+
+// complexity O(1) 
